4e3c24ca67827bcfab82e124d8c34230f3d6b46c
Gaston Roman <gaston_2104@hotmail.com>
2024-10-20 11:31:31 +0200

    Update philo.h

--- inc/philo.h
+++ inc/philo.h
@@ -30,20 +30,37 @@
 # define WRONG_ARGS	"Invalid number of arguments"
 # define INV_ARGS	"Invalid arguments"
 
+# define ACT_FORK "[%u] %d has taken a fork%s\n"
+# define ACT_EAT "\x1B[32m[%u] %d is eating%s\n"
+# define ACT_SLEEP "\x1B[36m[%u] %d is sleeping%s\n"
+# define ACT_THINK "\x1B[33m[%u] %d is thinking%s\n"
+# define ACT_DIE "\x1B[31m[%u] %d died%s\n"
+
+enum
+{
+	FORK,
+	EAT,
+	SLEEP,
+	THINK,
+	DIE,
+};
+
 typedef struct s_philo
 {
-	pthread_mutex_t		l_m_flag;
+	pthread_mutex_t		last_meal_flag;
 	pthread_mutex_t		chopstick1;
 	pthread_mutex_t		*chopstick2;
 	pthread_t			philo;
 	int					id;
-	int					t_eaten;
+	int					n_meals;
 	unsigned int		last_meal;
-	struct s_program	*program;
+	struct s_simulation	*sim;
 }	t_philo;
 
-typedef struct s_program
+typedef struct s_simulation
 {
+	pthread_mutex_t	action_flag;
+	pthread_mutex_t	check_flag;
 	t_philo			*philo;
 	int				flag_dead;
 	unsigned int	t_start;
@@ -51,9 +68,8 @@ typedef struct s_program
 	size_t			t_death;
 	size_t			t_eat;
 	size_t			t_sleep;
-	size_t			n_eat;
-	pthread_mutex_t	act_flag;
-}	t_program;
+	size_t			n_meals;
+}	t_simulation;
 
 /***********philo************/
 int		print_error(char *str, int type);

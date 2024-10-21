7f040565ea521b8dbd8832e3c1aea55b28e461b8
Gaston Roman <113539513+GastonRLdL@users.noreply.github.com>
2024-10-21 17:04:36 +0200

    Update program_utils.c

--- src/program_utils.c
+++ src/program_utils.c
@@ -21,34 +21,54 @@ unsigned int	get_current_time(void)
 	current_time = (unsigned int)tmp.tv_sec * 1000 + tmp.tv_usec * 0.001;
 	return (current_time);
 }
-void	_sleep(t_philo *philo)
+
+void	ft_sleep(t_philo *philo)
 {
 	print_action(philo, SLEEP, ACT_SLEEP);
 }
-void	_think(t_philo *philo)
+
+void	ft_think(t_philo *philo)
 {
 	print_action(philo, THINK, ACT_THINK);
 }
-void	_eat(t_philo *philo)
-{
 
+void	ft_eat(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->fork1);
+	print_action(philo, FORK, ACT_FORK);
+	if (&philo->fork1 != philo->fork2)
+	{
+		pthread_mutex_lock(philo->fork2);
+		print_action(philo, FORK, ACT_FORK);
+		pthread_mutex_lock(&philo->last_meal_flag);
+		philo->last_meal = get_sim_time(philo->sim);
+		philo->n_meals++;
+		pthread_mutex_unlock(&philo->last_meal_flag);
+		print_action(philo, EAT, ACT_EAT);
+		usleep(philo->sim->t_eat * 1000);
+		pthread_mutex_unlock(philo->fork2);
+		pthread_mutex_unlock(&philo->fork1);
+	}
+	else
+		usleep(philo->sim->t_death * 1000);
 }
+
 void	*routine(void *data)
 {
-	t_philo  *philo;
+	t_philo	*philo;
 
 	philo = data;
-	pthread_mutex_lock(&philo->program->act_flag);
-	pthread_mutex_unlock(&philo->program->act_flag);
-	if (chech_death(philo->program) && philo->t_eaten != philo->program->n_eat)
-		usleep((philo->program->t_eat -1) * 1000);
-	while (check_death(philo->program) && philo->t_eaten != philo->program->n_eat)
+	pthread_mutex_lock(&philo->sim->action_flag);
+	pthread_mutex_unlock(&philo->sim->action_flag);
+	if (chech_death(philo->sim) && philo->n_meals != philo->sim->n_meals)
+		usleep((philo->sim->t_eat -1) * 1000);
+	while (check_death(philo->sim) && philo->n_meals != philo->sim->n_meals)
 	{
-		_eat(philo);
-		if (philo->t_eaten  != philo->program->n_eat)
+		ft_eat(philo);
+		if (philo->n_meals != philo->sim->n_meals)
 		{
-			_sleep(philo);
-			_think(philo);
+			ft_sleep(philo);
+			ft_think(philo);
 		}
 	}
 	retun (0);

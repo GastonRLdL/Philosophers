871182a53c152c68c40a4a022925bd143e63e027
Gaston Roman <113539513+GastonRLdL@users.noreply.github.com>
2024-10-21 17:03:23 +0200

    Update philo.c

--- src/philo.c
+++ src/philo.c
@@ -12,63 +12,63 @@
 
 #include "../inc/philo.h"
 
-int	init_program(t_program *program)
+int	init_simultion(t_simulation *sim)
 {
 	int	i;
 
 	i = 0;
-	pthread_mutex_lock(&program->act_flag);
-	while (i < program->n_philo && !program->flag_dead)
+	pthread_mutex_lock(&sim->action_flag);
+	while (i < sim->n_philo && !sim->flag_death)
 	{
-		if (phtread_create(&program->philo[i].philo, NULL, \
-			&routine, &program->philo[i]))
+		if (pthread_create \
+		(&sim->philo[i].philo, NULL, &routine, &sim->philo[i]))
 		{
-			program->flag_dead = 1;
+			sim->flag_death = 1;
 			return (ERROR);
 		}
 		i++;
 	}
-	program->t_start = get_current_time();
-	pthread_mutex_unlock(&program->act_flag);
+	sim->t_start = get_current_time();
+	pthread_mutex_unlock(&sim->action_flag);
 	return (0);
 }
 
-int	prep_philos(t_program *program)
+int	prep_philos(t_simulation *sim)
 {
 	int	i;
 
-	program->philo = malloc(program->n_philo * sizeof(t_philo));
-	if (!program->philo)
+	sim->philo = malloc(sim->n_philo * sizeof(t_philo));
+	if (!sim->philo)
 		return (ERROR);
-	if (pthread_mutex_init(&program->act_flag, NULL))
+	if (pthread_mutex_init(&sim->action_flag, NULL))
 		return (ERROR);
 	i = -1;
-	while (++i < program->n_philo)
+	while (++i < sim->n_philo)
 	{
-		program->philo[i].id = i;
-		program->philo[i].t_eaten = 0;
-		program->philo[i].last_meal = 0;
-		program->philo[i].program = program;
-		if (pthread_mutex_init(&program->philo[i].chopstick1, NULL))
+		sim->philo[i].id = i;
+		sim->philo[i].n_meals = 0;
+		sim->philo[i].last_meal = 0;
+		sim->philo[i].sim = sim;
+		if (pthread_mutex_init(&sim->philo[i].fork1, NULL))
 			return (ERROR);
-		if (pthread_mutex_init(&program->philo[i].l_m_flag, NULL))
+		if (pthread_mutex_init(&sim->philo[i].last_meal_flag, NULL))
 			return (ERROR);
 		if (i > 0)
-			program->philo[i].chopstick2 = &program->philo[i - 1].chopstick1;
+			sim->philo[i].fork2 = &sim->philo[i - 1].fork1;
 	}
-	program->philo[0].chopstick2 = &program->philo[program->n_philo].chopstick1;
+	sim->philo[0].fork2 = &sim->philo[sim->n_philo].fork1;
 	return (SUCCESS);
 }
 
 int	main(int ac, char **av)
 {
-	t_program	program;
+	t_simulation	sim;
 
 	if (ac < 5 || ac > 6)
 		return (print_error(WRONG_ARGS, 1));
-	ft_bzero(&program, sizeof(program));
-	if (parse(ac, av, &program) == -1)
+	ft_bzero(&sim, sizeof(sim));
+	if (parse(ac, av, &sim) == -1)
 		return (print_error(INV_ARGS, 2));
-	init_program(&program);
+	init_simulation(&sim);
 	return (0);
 }
